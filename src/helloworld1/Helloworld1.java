9684242f3062f3a5d701ab5a7bc0fd009d77f949
g13901ae <g13901ae@s30439.tsuda.ac.jp>
2015-04-10 12:06:15 +0900

    FirstGit

--- src/helloworld1/Helloworld1.java
+++ src/helloworld1/Helloworld1.java
@@ -15,5 +15,6 @@ public class Helloworld1 {
      */
     public static void main(String[] args) {
         System.out.println("hello,world");// TODO code application logic here
+        System.out.print("!!!!!!!!!!");
     }
 }
