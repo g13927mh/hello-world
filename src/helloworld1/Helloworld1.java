ce3f3ce693aeda462008511a5d4db400c6d0c6ab
g13927mh <g13927mh@s30442.tsuda.ac.jp>
2015-04-10 11:16:31 +0900

    HelloWorldです。

---
+++ src/helloworld1/Helloworld1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld1;
+
+/**
+ *
+ * @author g13927mh
+ */
+public class Helloworld1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello,world");// TODO code application logic here
+    }
+}
